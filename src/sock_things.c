4f84d0db720aaa3b5505e498c301f705aea1ece2
vidkryvashka <rock097abc@gmail.com>
2025-03-15 16:49:31 +0000

    shows reverse hostnames where can find, a bit prettier refactored code, removed mallocs frees, no menu, time measurement yet

--- src/sock_things.c
+++ src/sock_things.c
@@ -31,37 +31,34 @@ struct packet_t {
 };
 
 
-static char *dns_lookup(const char *addr_host, struct sockaddr_in *addr_con) {
+static char *dns_lookup(const char *addr_host, struct sockaddr_in *addr_con, char *target_ip_addr) {
     struct hostent *host_entity;
-    char *ip = (char *)malloc(MY_NI_MAXHOST * sizeof(char));
     if (!(host_entity = gethostbyname(addr_host)))
-        return NULL;
+        return 0;
     
     addr_con->sin_family = host_entity->h_addrtype;
     addr_con->sin_port = htons(PORT_NUM);
     addr_con->sin_addr.s_addr = *(long *)host_entity->h_addr_list[0];
     
-    strcpy(ip, inet_ntoa(*(struct in_addr *)host_entity->h_addr_list[0]));
+    strcpy(target_ip_addr, inet_ntoa(*(struct in_addr *)host_entity->h_addr_list[0]));
 
-    return ip;
+    return 1;
 }
 
-static char *reverse_dns_lookup(const char *target_ip_addr) {
+static int reverse_dns_lookup(const char *target_ip_addr, char *dest) {
     struct sockaddr_in temp_addr;
     socklen_t len;
-    char buff[MY_NI_MAXHOST], *ret_buf;
+    char buff[MY_NI_MAXHOST];
 
     temp_addr.sin_family = AF_INET;
     temp_addr.sin_addr.s_addr = inet_addr(target_ip_addr);
 
     if (getnameinfo((struct sockaddr *)&temp_addr, sizeof(struct sockaddr_in), buff, sizeof(buff), NULL, 0, MY_NI_NAMEREQD)) {
-        printf("Error: could not resolve reverse lookup of hostname\n");
-        return NULL;
+        strcpy(dest, ".");
+        return 0;
     }
-
-    ret_buf = (char *)malloc((strlen(buff)+1) * sizeof(char));
-    strcpy(ret_buf, buff);
-    return ret_buf;
+    strcpy(dest, buff);
+    return 1;
 }
 
 
@@ -91,6 +88,31 @@ static void prepare_tx_pkt(struct packet_t *tx_pkt, int msg_sent) {
 }
 
 
+static int process_resp(char *rx_buff, int msg_sent, int ttl_binded) {
+
+    struct iphdr *ip_hdr = (struct iphdr *)rx_buff;
+    struct icmphdr *rx_icmp_hdr = (struct icmphdr *)(rx_buff + ip_hdr->ihl * 4);
+    char ip_addr[16];
+    char reverse_hostname[MY_NI_MAXHOST+1];
+    unsigned char *ip = (unsigned char *)&ip_hdr->saddr;
+    
+    if ( !(rx_icmp_hdr->type % 11)) {
+        printf("%d\t", msg_sent);
+        snprintf(ip_addr, 16, "%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
+        reverse_dns_lookup(ip_addr, reverse_hostname);
+        printf("%s\t%s\n",
+            ip_addr, reverse_hostname);
+            
+        if (rx_icmp_hdr->type == 11)
+            return 11;
+    } else {
+        printf("Error: responce icmp type %zu", rx_icmp_hdr->type);
+        return 0;
+    }
+    return 1;
+}
+
+
 static void fist_network(
         const int sockfd,
         const struct sockaddr_in *target_addr,
@@ -105,18 +127,14 @@ static void fist_network(
     };
     setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char *)&tv_out, sizeof(tv_out));
     
-    int is_pack_sent = 1, msg_sent = 0;
+    int is_pack_sent = 1, msg_sent = 0, ttl_binded = 0;
     struct packet_t tx_pkt;
     
-    
 move_deeper:
-    int ttl_binded = 1;
-    ttl_binded += msg_sent;
+    ttl_binded += msg_sent + 1;
     if (setsockopt(sockfd, SOL_IP, IP_TTL, &ttl_binded, sizeof(ttl_binded))) {
         printf("Error: setting socket options to TTL failed\n");
         return;
-    } else {
-        // printf("Socket set to TTL %d ...\n", ttl_binded);
     }
 
     prepare_tx_pkt(&tx_pkt, msg_sent);
@@ -148,34 +166,8 @@ move_deeper:
         printf("Error: packet receive failed\n");
     } else {
         if (is_pack_sent) {
-            struct iphdr *ip_hdr = (struct iphdr *)rx_buff;
-            struct icmphdr *rx_icmp_hdr = (struct icmphdr *)(rx_buff + ip_hdr->ihl * 4);
-            printf("%d\t", msg_sent);
-            char source[16];
-            unsigned char *ip = (unsigned char *)&ip_hdr->saddr;
-            switch (rx_icmp_hdr->type) {
-                case 0:         // echo reply
-                    snprintf(source, 16, "%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
-                    printf("%s\treach\n",
-                            source);
-                    // printf("%d bytes from %s (h: %s) (ip: %s) msg_seq = %d ttl = %d\n",
-                    //         TX_PACKET_SIZE, target_fqdn, rev_host, target_ip, msg_sent, ttl_binded);
-                    break;
-                case 11:
-                    snprintf(source, 16, "%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
-                    printf("%s\n",
-                            source);
-                    // printf("time exceeded %d bytes from %s (h: %s) (ip: %s) msg_seq = %d ttl = %d\n",
-                    //         TX_PACKET_SIZE, target_fqdn, rev_host, target_ip, msg_sent, ttl_binded);
-                    if (ttl_binded <= CUSTOM_MAX_HOP) {
-                        goto move_deeper;
-                    }
-                    break;
-                default:
-                    printf("Error: packet received with ICMP type %d code %d\n",
-                            rx_icmp_hdr->type, rx_icmp_hdr->code);
-                    break;
-            }
+            if (process_resp(rx_buff, msg_sent, ttl_binded) == 11)
+                goto move_deeper;
         }
     }
 }   // static void fist_network
@@ -184,18 +176,21 @@ move_deeper:
 int organize() {
 
     int sockfd;
-    char *target_ip_addr, *reverse_hostname;
+    char target_ip_addr[MY_NI_MAXHOST];
+    char reverse_hostname[MY_NI_MAXHOST+1];
     struct sockaddr_in addr_con;
     int addr_len = sizeof(addr_con);
 
-    target_ip_addr = dns_lookup(CUSTOM_FQDN, &addr_con);
-    if (target_ip_addr == NULL) {
+    
+    if (!dns_lookup(CUSTOM_FQDN, &addr_con, target_ip_addr)) {
         printf("Error: dns_lookup failed, couldn't resolve %s address\n", CUSTOM_FQDN);
         return 0;
     }
 
-    reverse_hostname = reverse_dns_lookup(target_ip_addr);
+    reverse_dns_lookup(target_ip_addr, reverse_hostname);
+
     printf("Target ip: %s, reverse lookup domain: %s\n", target_ip_addr, reverse_hostname);
+
 
     sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
     if (sockfd < 0) {
@@ -204,10 +199,6 @@ int organize() {
     }
 
     fist_network(sockfd, &addr_con, reverse_hostname, target_ip_addr, CUSTOM_FQDN);
-
-
-    free(target_ip_addr);
-    free(reverse_hostname);
 
     printf("end program\n");
 
